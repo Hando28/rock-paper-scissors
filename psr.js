e244b605d1c21465b3b655cdb026dd4d1fc421bc
Hando28 <154564801+SlickHando@users.noreply.github.com>
2025-01-24 18:27:15 -0500

    fix syntax for getHuman Choice and getComputerChoice. fix logic in playRound function and add score incrementation for humanScore and computerScore variables.

--- psr.js
+++ psr.js
@@ -1,40 +1,47 @@
+
+
 const choices = ["Rock", "Paper", "Scissors"];
+
+
 
 var humanScore = 0;
 var computerScore = 0;
 
-function getHumanChoice() {
-  let playerChoice = window.prompt("Choose one and enter below! \nRock, Paper, Scissors", "");
-  
-  if (playerChoice === "Rock" || playerChoice === "Paper" || playerChoice === "Scissors") {
-    console.log(playerChoice);
-  } else {
-    alert("Invalid input. Please try again.");
-  }
-}
+
 
 function getComputerChoice(choices) {
  let result = Math.floor(Math.random() * 3);
  return choices[result];
 }
 
-console.log(getComputerChoice(choices));
+
+
+function getHumanChoice() {
+  let playerChoice = window.prompt("Choose one and enter below! \nRock, Paper, Scissors", "");
+  
+  if (playerChoice === "Rock" || playerChoice === "Paper" || playerChoice === "Scissors") {
+    return playerChoice;
+  } else {
+    alert("Invalid input. Please try again.");
+  }
+}
+
 
 
 function playRound(humanChoice, computerChoice) { 
   
   if (humanChoice === "Rock" && computerChoice === "Paper") {
-    console.log("You lose! Paper covers Rock!");
+    console.log("You lose! Paper covers Rock!", ++computerScore);
   } else if (humanChoice === "Paper" && computerChoice === "Rock") {
-    console.log("You win! Paper covers Rock!");
+    console.log("You win! Paper covers Rock!", ++humanScore);
   } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
-    console.log("You lose! Rock beats Scissors!");
+    console.log("You lose! Rock beats Scissors!", ++computerScore);
   } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
-    console.log("You win! Rock beats Scissors!");
+    console.log("You win! Rock beats Scissors!", ++humanScore);
   } else if (humanChoice === "Paper" && computerChoice === "Scissors") {
-    console.log("You lose! Scissors cuts Paper!");
+    console.log("You lose! Scissors cuts Paper!", ++computerScore);
   } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
-    console.log("You win! Scissors cuts paper!");
+    console.log("You win! Scissors cuts paper!", ++humanScore);
   } else {
     console.log("It's a draw!");
   } 
@@ -43,4 +50,4 @@ function playRound(humanChoice, computerChoice) {
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice(choices);
 
-playRound(humanSelection, computerSelection);
+console.log(playRound(humanSelection, computerSelection));
